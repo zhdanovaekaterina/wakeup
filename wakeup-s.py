403530234c3f052605c943655dfc236f92f0b577
zhdanovaekaterina <93944449+zhdanovaekaterina@users.noreply.github.com>
2021-12-01 18:16:13 +0300

    01.12.21
    Добавлено приведение номеров телефонов к единому виду, загрузка ключевого столбца в дата фрейм и удаление ненужных столбцов
    TODO: добавить в ключевой столбец e-mail'ы; проверить корректность присвоения ключевых значений всем записям (отсутствие значения "-" в ключевом столбце).
    Возможно, имеет смысл объединять ключевое значение функцией mergeByElements - соединит все телефоны в один ключ через запятую, сопоставлять можно будет по одному значению из строки (разделять также через запятую).

--- wakeup-s.py
+++ wakeup-s.py
@@ -20,6 +20,13 @@ dl = dl[(~dl['Стадия сделки'].str.contains('[Тт]ест.*')) & (~dl
 dl = dl[(~dl['Название сделки'].str.contains('Заявка на демо-модуль'))]
 dl = dl[(dl['Направление'].str.contains('WakeUp')) | (dl['Направление'].str.contains('Обучение WakeUp'))]
 
+# Проверка данных сделок и лидов на наличие телефонов и e-mail. Оставляет только те записи, по которым есть телефон или e-mail. Остальные - выгружает в файл excel в текущем каталоге
+(ld[((ld['Рабочий телефон'].isna()) & (ld['Мобильный телефон'].isna()) & (ld['Другой телефон'].isna()) & (ld['Рабочий e-mail'].isna()) & (ld['Частный e-mail'].isna()) & (ld['Другой e-mail'].isna()))]).to_excel('leads_without_contact.xlsx')
+(dl[((dl['Телефон клиента (ОСУ)'].isna()) & (dl['Почта клиента (ОСУ)'].isna()) & (dl['Контакт: Рабочий телефон'].isna()) & (dl['Контакт: Мобильный телефон'].isna()) & (dl['Контакт: Другой телефон'].isna()) & (dl['Контакт: Рабочий e-mail'].isna()) & (dl['Контакт: Частный e-mail'].isna()) & (dl['Контакт: Другой e-mail'].isna()))]).to_excel('deals_without_contact.xlsx')
+
+ld = ld[~((ld['Рабочий телефон'].isna()) & (ld['Мобильный телефон'].isna()) & (ld['Другой телефон'].isna()) & (ld['Рабочий e-mail'].isna()) & (ld['Частный e-mail'].isna()) & (ld['Другой e-mail'].isna()))]
+dl = dl[~((dl['Телефон клиента (ОСУ)'].isna()) & (dl['Почта клиента (ОСУ)'].isna()) & (dl['Контакт: Рабочий телефон'].isna()) & (dl['Контакт: Мобильный телефон'].isna()) & (dl['Контакт: Другой телефон'].isna()) & (dl['Контакт: Рабочий e-mail'].isna()) & (dl['Контакт: Частный e-mail'].isna()) & (dl['Контакт: Другой e-mail'].isna()))]
+
 # Выгрузка столбцов, содержащих имя, в списки
 clientName = ld['Имя'].tolist()
 clientSecondName = ld['Отчество'].tolist()
@@ -63,10 +70,91 @@ def mergeByElements(listOfLists:list, goalList:list, delimiterList:str):
 
 mergeByElements(listOfNames, clientContact, ' ')
 ld['Контакт'] = clientContact #добавление колонки в дата фрейм
+ld = ld.drop('Имя', 1) #удаление лишних колонок
+ld = ld.drop('Отчество', 1)
+ld = ld.drop('Фамилия', 1)
 
 # Выгрузка столбцов, содержащих телефон, в списки
 workTel = ld['Рабочий телефон'].tolist()
 mobTel = ld['Мобильный телефон'].tolist()
 otherTel = ld['Другой телефон'].tolist()
+listOfRawTelephones = [otherTel, workTel, mobTel]
 
-# Приведение номера телефона к одному виду
+workTelDl = dl['Контакт: Рабочий телефон'].tolist()
+mobTelDl = dl['Контакт: Мобильный телефон'].tolist()
+otherTelDl = dl['Контакт: Другой телефон'].tolist()
+osuTelDl = dl['Телефон клиента (ОСУ)'].tolist()
+listOfRawTelephonesDl = [otherTelDl, osuTelDl, workTelDl, mobTelDl]
+
+# Явное преобразование всех номеров телефонов в строки
+for telItem in listOfRawTelephones:
+    typeChange(telItem)
+
+for telItem in listOfRawTelephonesDl:
+    typeChange(telItem)
+
+# Приведение номеров телефонов к единому формату и запись в общий список (важна запись в порядке перебора списков при записи id по номеру)
+newWorkTel = [''.join(filter(str.isdigit, tel)) for tel in workTel]
+newMobTel = [''.join(filter(str.isdigit, tel)) for tel in mobTel]
+newOtherTel = [''.join(filter(str.isdigit, tel)) for tel in otherTel]
+listOfTelephones = [newOtherTel, newWorkTel, newMobTel]
+
+newWorkTelDL = [''.join(filter(str.isdigit, tel)) for tel in workTelDl]
+newMobTelDl = [''.join(filter(str.isdigit, tel)) for tel in mobTelDl]
+newOtherTelDl = [''.join(filter(str.isdigit, tel)) for tel in otherTelDl]
+newOsuTelDl = [''.join(filter(str.isdigit, tel)) for tel in osuTelDl]
+listOfTelephonesDl = [newOtherTelDl, newOsuTelDl, newWorkTelDL, newMobTelDl]
+
+keyTelLd = []
+keyTelDl = []
+
+def mergeTelephones(listOfLists:list, goalList:list):
+    '''Принимает список списков телефонов, из которых необходимо оставить ключевой номер.
+        Проверяет входные списки на одинаковую длину.
+        Возвращает список номеров телефонов, собранный из всех списков.
+        Важно передавать списки в порядке приоритета.
+    '''
+    flag = False
+    if goalList != []:
+        return 'Ошибка: в целевом списке содержатся данные'
+    lenthInner = len(listOfLists[0]) #длина внутреннего списка
+    for i in range(1, len(listOfLists)):
+        if len(listOfLists[i]) != lenthInner:
+            flag = True
+    if flag:
+        return 'Ошибка: переданные списки разной длины'
+    for k in range(0, lenthInner):
+        n = 0
+        while n <= len(listOfLists):
+            if n != len(listOfLists) and listOfLists[n][k] != '':
+                goalList.append(listOfLists[n][k])
+                n += 1
+            else:
+                goalList.append('-')
+                break
+
+mergeTelephones(listOfTelephones, keyTelLd)
+mergeTelephones(listOfTelephonesDl, keyTelDl)
+
+# Проверка длины ключа
+
+def lenthValidation(listToCheck:list):
+    for i in range(0, len(listToCheck)):
+        temp = listToCheck[i]
+        if len(temp) > 13 and temp[0] == 7:
+            listToCheck[i] = temp[12]
+
+lenthValidation(keyTelLd)
+lenthValidation(keyTelDl)
+
+# Загрузка ключевого телефона в дата фреймы
+# НЕТ ПРОВЕРКИ ОДИНАКОВОЙ ДЛИНЫ КЛЮЧЕВОГО СТОЛБЦА И ЦЕЛЕВОГО ДАТА ФРЕЙМА
+ld['Ключевой телефон'] = keyTelLd
+dl['Ключевой телефон'] = keyTelDl
+ld = ld.drop('Рабочий телефон', 1) #удаление лишних колонок
+ld = ld.drop('Мобильный телефон', 1)
+ld = ld.drop('Другой телефон', 1)
+dl = dl.drop('Контакт: Рабочий телефон', 1)
+dl = dl.drop('Контакт: Мобильный телефон', 1)
+dl = dl.drop('Контакт: Другой телефон', 1)
+dl = dl.drop('Телефон клиента (ОСУ)', 1)
